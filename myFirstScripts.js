3ff28041332a90b622e81ff6eaa719cef9b1081f
Bora <62065271+Borab2001@users.noreply.github.com>
2023-03-26 11:30:55 +0200

    Form validation function

--- myFirstScripts.js
+++ myFirstScripts.js
@@ -1,16 +1,30 @@
 function validateMyForm() {
-    // Get form elements
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const birthday = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-  
-    // Check if form elements are empty
-    if (firstName === "" || lastName === "" || email === "" || message === "") {
-      // Display alert box with missing fields
-      alert("Please fill in all fields.");
-    } else {
-      // Display alert box with form summary
-      alert(`First Name: ${firstName}\Last Name: ${lastName}\nEmail: ${email}\nMessage: ${message}`);
-    }
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const birthday = document.getElementById("birthday").value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+
+  if (!firstName || !lastName || !birthday || !gender) {
+      let message = "";
+      if (!firstName) {
+      message += "- First Name\n";
+      }
+      if (!lastName) {
+      message += "- Last Name\n";
+      }
+      if (!birthday) {
+      message += "- Birthday\n";
+      }
+      if (!gender) {
+      message += "- Gender\n";
+      }
+      alert(`Please fill out the following fields:\n${message}`);
+      return false;
+  }
+
+  // If all fields are filled out, display an alert with the form summary
+  const summary = `First Name: ${firstName}\nLast Name: ${lastName}\nBirthday: ${birthday}\nGender: ${gender.value}`;
+  alert(summary);
+
+  return true;
 }
